3c49ea09b7576deea1b0a49d13580d9f08960f0b
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-08 22:34:39 -0500

    Task 4: conditionals and dynamic messages

--- JavaScript - Week #1/interactive_system.js
+++ JavaScript - Week #1/interactive_system.js
@@ -10,3 +10,10 @@ while (isNaN(age)) {
     console.error("\nError: Please, enter a valid age in numbers.");
     age = prompt("Enter your age: ");
 } 
+
+// Conditionals and dynamic messages
+if (age < 18) {
+    console.log(`\nHi ${name}, you're underage. Keep learning and enjoying coding!`);
+} else {
+    console.log(`\nHello ${name}, you are of legal age. Get ready for great opportunities in the world of programming!`);
+}
