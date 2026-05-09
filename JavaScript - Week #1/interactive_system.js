8b805d24f91c97a5bebd2c894f574d9a54a81546
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-08 22:30:44 -0500

    Task 3: age validation

--- JavaScript - Week #1/interactive_system.js
+++ JavaScript - Week #1/interactive_system.js
@@ -4,3 +4,9 @@ const prompt = require("prompt-sync")()
 // User data entry
 let name = prompt("Enter your name: ");
 let age = prompt("Enter your age: ");
+
+// Age validation
+while (isNaN(age)) {
+    console.error("\nError: Please, enter a valid age in numbers.");
+    age = prompt("Enter your age: ");
+} 
