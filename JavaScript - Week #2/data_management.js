75d80f66371092ccfcb34703dafd6f724340b336
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-15 15:52:22 -0500

    Task #1: objects

---
+++ JavaScript - Week #2/data_management.js
@@ -0,0 +1,11 @@
+// Task #1: creation of the product object
+let product = {
+    id: 1,                  // Product ID
+    productName: "apple",   // Product name
+    price: 2500             // Product price
+}
+// Show the complete object in the console
+console.log(product);
+
+
+// Task #2: using Set in JavaScript
