e906c81b7c98c65c4373eec9aea3005ea4a297f2
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-22 17:59:23 -0500

    Task #2: selection and inspection

---
+++ JavaScript - Week #3/script.js
@@ -0,0 +1,10 @@
+// Gets the html elements
+const input = document.getElementById('noteInput');
+const button = document.getElementById('button');
+const list = document.getElementById('listNotes');
+
+// Displays the elements
+console.log(input);
+console.log(button);
+console.log(list);
+
