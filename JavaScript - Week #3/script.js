3b317ad219c13ed015ee1fc83fc5ca9dfbcf1ce1
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-22 19:34:21 -0500

    Task #4: delete notes to the dom

--- JavaScript - Week #3/script.js
+++ JavaScript - Week #3/script.js
@@ -8,7 +8,9 @@ console.log(input);
 console.log(button);
 console.log(list);
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function(event) {
+
+    event.preventDefault();
 
     // Validate if the input is empty
     if (input.value === "") {
@@ -25,6 +27,11 @@ button.addEventListener('click', function() {
     li.appendChild(deleteButton);                             // Insert the button into the <li>
     list.appendChild(li);                                     // Insert the <li> into the <ul>
 
-    console.log('Note added');                                // Print message in console
+    console.log(input);                                       // Print message in console
     input.value = '';                                         // Clear the input
+
+    deleteButton.addEventListener('click', function(){
+        console.log(li);
+        list.removeChild(li);                                 // Remove the <li> from de list
+    })
 });
