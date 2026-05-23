da0681f996cd6d61c4bd1705e7dfb61f854aa3c7
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-22 19:14:46 -0500

    Task #3: add notes to the dom

--- JavaScript - Week #3/script.js
+++ JavaScript - Week #3/script.js
@@ -8,3 +8,23 @@ console.log(input);
 console.log(button);
 console.log(list);
 
+button.addEventListener('click', function() {
+
+    // Validate if the input is empty
+    if (input.value === "") {
+        alert('Please, write a note');
+        return;
+    }
+
+    const li = document.createElement('li');                  // Create the <li>
+    const deleteButton = document.createElement('button');    // Create the delete button
+    
+    li.textContent = input.value;                             // Add the note text
+    deleteButton.innerHTML = 'x';                             // Add text to the button
+
+    li.appendChild(deleteButton);                             // Insert the button into the <li>
+    list.appendChild(li);                                     // Insert the <li> into the <ul>
+
+    console.log('Note added');                                // Print message in console
+    input.value = '';                                         // Clear the input
+});
